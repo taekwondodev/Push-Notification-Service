284603df5194fc79f0d40f26a4c8a32e58ab9587
taekwondodev <taekwondodev@outlook.it>
2025-06-01 21:57:51 +0200

    fix format

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,26 +12,26 @@ import (
 )
 
 func main() {
-    cfg := config.Load()
+	cfg := config.Load()
 
-    repo := repository.NewMongoNotificationRepository(cfg.Mongo.URI, cfg.Mongo.Database)
-    defer repo.Close()
-    
-    notifService := service.NewNotificationService(repo)
-    hub := websocket.NewHub()
-    kafkaService := service.NewKafkaService(&cfg.Kafka, hub, notifService)
-    
-    notifController := controller.NewNotificationController(notifService, kafkaService)
-    wsController := controller.NewWebSocketController(hub)
-    
-    ctx, cancel := context.WithCancel(context.Background())
-    go func () {
-        if err := kafkaService.StartConsumer(ctx); err != nil {
-            cancel()
-        }
-    }()
-    
-    router := api.SetupRoutes(notifController, wsController)
-    server := api.NewServer(cfg.Server.Port, router)
-    server.StartWithGracefulShutdown()
+	repo := repository.NewMongoNotificationRepository(cfg.Mongo.URI, cfg.Mongo.Database)
+	defer repo.Close()
+
+	notifService := service.NewNotificationService(repo)
+	hub := websocket.NewHub()
+	kafkaService := service.NewKafkaService(&cfg.Kafka, hub, notifService)
+
+	notifController := controller.NewNotificationController(notifService, kafkaService)
+	wsController := controller.NewWebSocketController(hub)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		if err := kafkaService.StartConsumer(ctx); err != nil {
+			cancel()
+		}
+	}()
+
+	router := api.SetupRoutes(notifController, wsController)
+	server := api.NewServer(cfg.Server.Port, router)
+	server.StartWithGracefulShutdown()
 }
